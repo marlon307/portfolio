3d59ea7c82264fe810bf7c0678de38ac978a731e
marlon307 <marlon-ramosb@hotmail.com>
2021-11-23 06:22:59 -0300

    Rename file backgrond1

---
+++ components/BackGrounds/Background1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Background = function Background() {
+  return (
+    <div className={ style.background }>
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default Background;

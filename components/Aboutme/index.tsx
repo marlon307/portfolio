314151d0886c908c84998d37008d1b563b5e7649
marlon307 <marlon-ramosb@hotmail.com>
2021-11-23 07:02:07 -0300

    Update projects

--- components/Aboutme/index.tsx
+++ components/Aboutme/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import About from './About';
 import style from './style.module.scss';
 import photo from '../../assets/download.jpeg';
-import AboutCardCircle from './AboutCardCircle';
 
 function index() {
   return (
@@ -13,9 +12,7 @@ function index() {
         title="Marlon Ramos"
         paragraph="Atualmente estudo desenvolvimento web Full-Stack em busca de realocação no mercado da tecnologia."
       />
-      <div className={ style.aboutmeinfos }>
-        <AboutCardCircle />
-      </div>
+      <div className={ style.aboutmeinfos } />
     </section>
   );
 }

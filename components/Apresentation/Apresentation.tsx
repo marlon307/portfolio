a2b8d338750f799aa310f9837c67a6695de8cc5b
_marlon307 <marlon-ramosb@hotmail.com>
2021-11-28 17:56:30 -0300

    Update Apresentation.tsx

--- components/Apresentation/Apresentation.tsx
+++ components/Apresentation/Apresentation.tsx
@@ -5,6 +5,7 @@ import style from './style.module.scss';
 const Apresentation = function Apresentation() {
   return (
     <section className={ style.apresentation }>
+      <h2>Site em manutenção &#x1F6A7;</h2>
       <Me />
       <h2>Site em manutenção &#x1F6A7;</h2>
     </section>

8970e80740743755ed7e16bce899d9e8de6ff36a
marlon307 <marlon-ramosb@hotmail.com>
2021-12-16 16:09:56 -0300

    Update card info dynamic image

--- components/PProject/Project.tsx
+++ components/PProject/Project.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from './Card';
 import style from './style.module.scss';
+import teste from '../../assets/icons8-futurama-bender.svg';
 
 const Project = function Project() {
   return (
     <section className={ style.projects }>
       <h1>Projetos.</h1>
       <div className={ style.mural }>
-        <Card number="1" name="Projeto Respponse" />
-        <Card number="2" name="HowlingDawn" />
+        <Card url={ teste } number="1" name="Projeto Respponse" />
+        <Card url={ teste } number="2" name="HowlingDawn" />
       </div>
     </section>
   );

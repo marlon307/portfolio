4a8fe1a40bf937fb25c9d3a047218ee830dbab70
marlon307 <marlon-ramosb@hotmail.com>
2021-12-16 17:25:58 -0300

    Responsive project

--- components/PProject/Project.tsx
+++ components/PProject/Project.tsx
@@ -6,9 +6,12 @@ import teste from '../../assets/icons8-futurama-bender.svg';
 const Project = function Project() {
   return (
     <section className={ style.projects }>
-      <h1>Projetos.</h1>
+      <div className={ style.titlesection }>
+        <h1>Projetos.</h1>
+        <span>Projetos próprios e quais eu já participei.</span>
+      </div>
       <div className={ style.mural }>
-        <Card url={ teste } number="1" name="Projeto Respponse" />
+        <Card url={ teste } number="1" name="Respponse" />
         <Card url={ teste } number="2" name="HowlingDawn" />
       </div>
     </section>

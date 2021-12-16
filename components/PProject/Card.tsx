56bbb4469a36c26a530fdbf2d44b3b94e7aed5d0
marlon307 <marlon-ramosb@hotmail.com>
2021-12-16 15:35:05 -0300

    Creat gid projects add files

---
+++ components/PProject/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Card = function Card() {
+  return (
+    <div className={ style.cardproject }>
+      <div>
+        <p>Nome do projeto</p>
+        <h2>1</h2>
+      </div>
+      <div>
+        sdsd
+      </div>
+    </div>
+  );
+};
+
+export default Card;

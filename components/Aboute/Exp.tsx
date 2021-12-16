c8be20352ce9fc284a8ec6493c4a02057896e887
marlon307 <marlon-ramosb@hotmail.com>
2021-12-16 13:36:49 -0300

    Update aboute experiences info add tooltip

--- components/Aboute/Exp.tsx
+++ components/Aboute/Exp.tsx
@@ -5,9 +5,12 @@ export type TExpProps = {
   year?: number;
   office: string;
   institution: string;
+  info: string | React.ReactElement;
 }
 
-const Exp = function Exp({ year, office, institution }: TExpProps) {
+const Exp = function Exp({
+  year, office, institution, info,
+}: TExpProps) {
   return (
     <div className={ style.cont }>
       <div className={ style.pl }>
@@ -17,11 +20,9 @@ const Exp = function Exp({ year, office, institution }: TExpProps) {
         <h3>{ office }</h3>
         <span>{ institution }</span>
       </div>
-      <div className={ style.info }>
-        <aside>
-          sadssssssssssssss
-        </aside>
-      </div>
+      <span className={ style.info }>
+        { info }
+      </span>
     </div>
   );
 };

27605038dd024f77d11ae8ce3a3fbb5c9beef08d
marlon307 <marlon-ramosb@hotmail.com>
2021-12-17 16:54:43 -0300

    Finish modal

--- components/Modal/ContentModal.tsx
+++ components/Modal/ContentModal.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import useOutSideClick from '../../Hooks/useOutSideClick';
+import style from './style.module.scss';
 
 type ContentModalProps = {
   children: React.ReactNode
+  active: boolean;
+  functionTogle: Function;
 }
 
-const ContentModal = function ContentModal({ children }: ContentModalProps) {
+const ContentModal = function ContentModal({ children, active, functionTogle }: ContentModalProps) {
   if (typeof window === 'undefined') return null;
 
   const getModal = document.getElementById('modal')!;
+  const modalRef = useRef(null);
+
+  useOutSideClick(modalRef, () => {
+    if (active) functionTogle(false);
+  });
+
+  useEffect(() => {
+    if (active) {
+      getModal.classList.add(style.active);
+    } else {
+      getModal.classList.remove(style.active);
+    }
+  }, [active, getModal.classList]);
 
   return (
-    ReactDOM.createPortal(children, getModal)
+    getModal
+      ? ReactDOM.createPortal(
+        <div className={ style.content } ref={ modalRef }>
+          { active && children }
+        </div>,
+        getModal,
+      )
+      : null
   );
 };
 
--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import cx from 'classnames';
 import style from './style.module.scss';
 
-type ModalProps = {
-  active: boolean;
-}
-
-const Modal = function Modal({ active }: ModalProps) {
+const Modal = function Modal() {
   return (
     <div
       id="modal"
-      className={ cx(style.modal, {
-        [style.active]: active,
-      }) }
+      className={ style.modal }
     />
   );
 };

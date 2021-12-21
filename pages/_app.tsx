f2919159df30db55fa2508aca9669e904bb68ba7
marlon307 <marlon-ramosb@hotmail.com>
2021-12-21 10:03:44 -0300

    Update SEO meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,14 +10,17 @@ const MyApp = function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Marlon</title>
         <meta name="description" content="Portfólio Marlon" />
-        <meta property="og:title" content="Sobre - Marlon" />
+        <meta property="og:title" content="Marlon" />
         <meta property="og:description" content="Portfólio Marlon307" />
         <meta property="og:url" content="https://marlon307.dev/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="../assets/image.svg" />
+        <meta property="og:image" content="/image.svg" />
         <meta name="twitter:url" content="https://twitter.com/_marlon307" />
         <meta name="linkedin:url" content="https://www.linkedin.com/in/marlon307/" />
         <meta name="marlon307:url" content="https://github.com/marlon307/" />
+        <meta name="author" content="Marlon" />
+        <meta name="title" content="Marlon" />
+        <meta name="keywords" content="Marlon, marlon, Marlon307, marlon307, _marlon307" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />

954e9b0aed07888b0b66876235887cd8af5fca69
marlon307 <marlon-ramosb@hotmail.com>
2021-12-21 09:39:43 -0300

    Update SEO og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
+import image from '../assets/image.svg';
 
 const MyApp = function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,14 @@ const MyApp = function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Marlon</title>
         <meta name="description" content="Portfólio Marlon" />
+        <meta property="og:title" content="Sobre - Marlon" />
+        <meta property="og:description" content="Portfólio Marlon307" />
+        <meta property="og:url" content="https://marlon307.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={ image } />
+        <meta name="twitter:url" content="https://twitter.com/_marlon307" />
+        <meta name="linkedin:url" content="https://www.linkedin.com/in/marlon307/" />
+        <meta name="marlon307:url" content="https://github.com/marlon307/" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
